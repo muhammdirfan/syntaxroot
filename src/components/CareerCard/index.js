8ffdbf11c2508b587f6a68fac0806c0e13c37e36
= <=>
2023-03-10 15:00:27 +0500

    test commit

--- src/components/CareerCard/index.js
+++ src/components/CareerCard/index.js
@@ -3,10 +3,12 @@ import React from "react";
 const CareerCard = ({ item, bgColor }) => {
   return (
     <div
-      className={`${bgColor} text-center py-16 px-5 career-card`}
+      className={`${bgColor} text-center py-14 px-5 career-card`}
       style={{ boxShadow: "0px 20px 29px -14px rgba(40, 55, 74, 0.08)" }}
     >
-      <h2 className="text-dark50 text-2xl mx-auto text-center">{item.name}</h2>
+      <h2 className="text-dark50 text-2xl mx-auto text-center py-2">
+        {item.name}
+      </h2>
       <p className="text-sm text-gray100">{item.para}</p>
     </div>
   );
